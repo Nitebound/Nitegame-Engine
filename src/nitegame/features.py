18255fd26d0f14b1f76ab27ab1b8fbb80105468f
Zachery Visger <zvisger@gmail.com>
2022-12-09 07:52:36 -0800

    Test Commit

--- src/nitegame/features.py
+++ src/nitegame/features.py
@@ -1,5 +1,76 @@
 """ UI Features """
 import pygame as pg
+from .core import COLORS
 
-DEFAULT_FONT = pg.font.get_default_font()
-print(DEFAULT_FONT)
+pg.font.init()
+DEFAULT_FONT = "Times New Roman"
+
+
+class UILabel:
+    def __init__(self, text, font_size=80, font_color=COLORS["black"], bg_color=None):
+        self.font = pg.font.SysFont(DEFAULT_FONT, font_size)
+        self._font_size = font_size
+        self._font_color = font_color
+        self._bg_color = bg_color
+        self._text = text
+        self.surface = self.font.render(self._text, True, self._font_color, self._bg_color)
+        self._font_path = None
+
+    def set_sys_font(self, font_name=None, font_size=None):
+        if not font_size:
+            font_size = self._font_size
+        else:
+            self._font_size = font_size
+
+        self.font = pg.font.SysFont(font_name, font_size)
+        self.surface = self.font.render(self.text, True, self._font_color, self._bg_color)
+
+    def set_ttf_font(self, font_path=None, font_size=None):
+        if not font_path:
+            font_path = self._font_path
+        else:
+            self._font_path = font_path
+
+        if not font_size:
+            font_size = self._font_size
+        else:
+            self._font_size = font_size
+
+        self.font = pg.font.Font(font_path, font_size)
+        self.surface = self.font.render(self.text, True, self._font_color, self._bg_color)
+
+    def set_bold(self, value=True):
+        self.font.set_bold(value)
+        self.surface = self.font.render(self._text, True, self._font_color, self._bg_color)
+
+    def set_underline(self, value=True):
+        self.font.set_underline(value)
+        self.surface = self.font.render(self._text, True, self._font_color, self._bg_color)
+
+    @property
+    def font_color(self, color):
+        self._font_color = color
+        self.surface = self.font.render(self.text, True, self._font_color, self._bg_color)
+
+    @font_color.getter
+    def font_color(self):
+        return self._font_color
+
+    @font_color.setter
+    def font_color(self, color):
+        self._font_color = color
+        self.surface = self.font.render(self.text, True, self._font_color, self._bg_color)
+
+    @property
+    def text(self, value):
+        self._text = value
+        self.surface = self.font.render(self._text, True, self._font_color, None)
+
+    @text.getter
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        self._text = value
+        self.surface = self.font.render(self._text, True, self._font_color, COLORS["black"])
--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,7 +1,18 @@
 from src.nitegame.core import *
+from src.nitegame.features import *
+from pathlib import Path
+
+font_dir = Path("assets/fonts")
 
 display = PGDisplay()
 running = True
+
+font_label = UILabel("Hello to the GUI world!")
+font_label.set_ttf_font(font_dir / "astutelight.ttf", 20)
+
+name_label = UILabel("Zachery", 30, COLORS["red"])
+name_label.set_sys_font("Times New Roman")
+name_label.font_color = COLORS["blue"]
 
 while running:
     events = pg.event.get()
@@ -16,5 +27,6 @@ while running:
 
     # DRAW
     display.clear()
-
+    display.blit(font_label.surface, (display.get_width()/2 - font_label.surface.get_width()/2, 110))
+    display.blit(name_label.surface, (display.get_width()/2 - font_label.surface.get_width()/2, 300))
     display.update()
